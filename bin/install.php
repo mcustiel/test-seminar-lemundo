1882dba2ab743f25c0074235cfe50483d3a278a4
Mariano Custiel <jmcustiel@gmail.com>
2019-05-19 21:43:04 +0200

    Improved install script

--- bin/install.php
+++ bin/install.php
@@ -3,9 +3,9 @@
 const BASE_PATH = __DIR__ . '/..';
 require BASE_PATH . '/vendor/autoload.php';
 
-use Lemundo\Translator\Factory;
 use Dotenv\Dotenv;
 use Lemundo\Translator\Config;
+use Lemundo\Translator\Factory;
 
 $dotenv = Dotenv::create(BASE_PATH);
 $dotenv->load();
@@ -13,7 +13,16 @@ $dotenv->load();
 $config = new Config(require BASE_PATH . '/config/config.php');
 
 $factory = new Factory($config);
+$dbFile = str_replace('sqlite:', '', $config->getDatabaseConnectionDsn());
+echo 'Checking database file ' . $dbFile . '...' . PHP_EOL;
+if (!file_exists($dbFile)) {
+    echo 'Creating database...' . PHP_EOL;
+    touch($dbFile);
+    $database = new PDO($config->getDatabaseConnectionDsn());
 
-$database = new PDO($config->getDatabaseConnectionDsn());
-$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$database->exec(file_get_contents(BASE_PATH . '/resources/install.sql'));
+    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $database->exec(file_get_contents(BASE_PATH . '/resources/install.sql'));
+    echo 'Database created...' . PHP_EOL;
+} else {
+    echo 'Database exists' . PHP_EOL;
+}
--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@
 
 docker-compose build
 docker-compose up -d
-docker-compose exec app ./bin/install
-
+./docker/bin/install

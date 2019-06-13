d93892978e8ed28262e035c8f524014d73882297
Mariano Custiel <m.custiel@lemundo.de>
2019-06-13 15:23:30 +0200

    Assert database values

--- codeception/tests/api/ApiCest.php
+++ codeception/tests/api/ApiCest.php
@@ -8,5 +8,7 @@ class ApiCest
         $I->sendPUT('/locale/de_DE/translation', ['text' => 'le tomato', 'id' => 'tomato']);
         $I->seeResponseCodeIs(201);
         $I->seeResponseEquals('{"id":"tomato","locale":"de_DE","text":"le tomato"}');
+        $I->seeInDatabase('translations', ['translation_id' => 'tomato', 'locale' => 'de_DE', 'text' => 'le tomato']);
+        $I->seeNumRecords(1, 'translations');
     }
 }
